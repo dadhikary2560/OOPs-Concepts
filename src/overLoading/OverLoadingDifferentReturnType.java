49a78ed7dfbc556696a5e5af62a59be69c9944e2
dadhikary2560 <d.adhikary2560@gmail.com>
2026-02-03 01:09:23 +0530

    overloading when we use different return types

--- src/overLoading/OverLoadingDifferentReturnType.java
+++ src/overLoading/OverLoadingDifferentReturnType.java
@@ -1,5 +1,24 @@
 package overLoading;
 
 public class OverLoadingDifferentReturnType {
+	int a;
+	public int m1(int b) {
+		System.out.println("value of a: "+a); //a is 0 given by default constructor
+		return a+b;
+	}
+	public String m1() {
+		return "Hello";
 
+	}
+	public void m1(byte a) {
+		System.out.println("taking byte as input");
+	}
+	
+	
+public static void main(String[] args) {
+	OverLoadingDifferentReturnType obj=new OverLoadingDifferentReturnType();
+	System.out.println(obj.m1(5));
+	System.out.println(obj.m1());
+	obj.m1((byte)4);					//need to write byte, as java take any value as int
+}
 }
