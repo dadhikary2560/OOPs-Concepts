eb276bbd52782c9d8ab66a322e87d3dd60b77540
dadhikary2560 <d.adhikary2560@gmail.com>
2026-02-02 19:53:58 +0530

    constructor demo

---
+++ src/OOPSConcept/ConstructorParent.java
@@ -0,0 +1,17 @@
+package OOPSConcept;
+
+public class ConstructorParent {
+ protected ConstructorParent() 
+ {
+	System.out.println("hi i am parent");
+ }
+ public void m1() {
+	System.out.println("hi! i am parent method");
+
+}
+}
+
+/*
+protected - same package
+			- child class, if different package
+*/
