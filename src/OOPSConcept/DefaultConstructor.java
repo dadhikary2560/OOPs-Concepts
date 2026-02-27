df62921638eee923a868ee57abbcfb706368f688
dadhikary2560 <d.adhikary2560@gmail.com>
2026-02-28 00:54:48 +0530

    Default constructor

---
+++ src/OOPSConcept/DefaultConstructor.java
@@ -0,0 +1,10 @@
+package OOPSConcept;
+
+public class DefaultConstructor {
+	public int a;
+	
+public static void main(String[] args) {
+	DefaultConstructor obj=new DefaultConstructor();//constructor invoking
+	System.out.println(obj.a);
+}
+}
