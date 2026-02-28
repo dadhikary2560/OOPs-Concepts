22b3f8d39b12ff3b2017a9361b183fe61df39376
dadhikary2560 <d.adhikary2560@gmail.com>
2026-03-01 00:56:31 +0530

    Abstraction parent IMPORTANT

---
+++ src/abstraction/AbstractParent_IMP.java
@@ -0,0 +1,16 @@
+package abstraction;
+
+public abstract class AbstractParent_IMP {
+
+	int x;
+	
+	public abstract void m1();
+	public abstract void m2();
+	public abstract void m3();
+	public abstract void m4();
+	public abstract void m5();
+	public abstract void m6();
+	
+	//all of the above methods are abstract methods
+}
+
