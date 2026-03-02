75508f056080249018b7f67cfa603f6fa69c8a15
dadhikary2560 <d.adhikary2560@gmail.com>
2026-03-03 01:37:44 +0530

    implementing parent class's method

---
+++ src/interfacePractice/InterfaceParent3Child.java
@@ -0,0 +1,8 @@
+package interfacePractice;
+
+public class InterfaceParent3Child implements InterfaceParent3_toInheritAnotherInterface{
+public static void main(String[] args) {
+	InterfaceParent3Child obj=new InterfaceParent3Child();
+	obj.m1();
+}
+}
