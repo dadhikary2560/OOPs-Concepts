6cc125913581ce6bacdf5d0b7f0d7927a6289f89
dadhikary2560 <d.adhikary2560@gmail.com>
2026-03-03 01:34:31 +0530

    overriding complete concepts

---
+++ src/OverRiding/OverridingUpdated_parent.java
@@ -0,0 +1,17 @@
+package OverRiding;
+
+public class OverridingUpdated_parent {
+	public OverridingUpdated_parent() {
+		// constructor of the parent class
+	}
+	protected void m1() {
+		System.out.println("parent method m1");
+	}
+
+	void m2() {
+		System.out.println("this is parent method m2");
+	}
+	static void m3() {
+		System.out.println("this is static method of parent class");
+	}
+}
